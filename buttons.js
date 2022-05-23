dfdb1394ddbca2b19b88d290199dfd39f131b504
AkmalAkhmetova1999 <amalka99@mail.ru>
2022-05-23 11:39:01 +0300

    SEO changes

--- buttons.js
+++ buttons.js
@@ -1,56 +1,58 @@
+//screen size>768px
 function seeMenu() {
-    if (document.getElementById("menu").style.display == "none") {
-        document.getElementById("menu").style = "display:block";
-    } else {
-        document.getElementById("menu").style = "display:none";
-    }
-
-}
-window.onclick = function (event) {
-    if (!event.target.matches('.btn')) {
-        var dropdowns = document.getElementById("menu");
-        if (dropdowns.style.display == "block") {
-            dropdowns.style = "display:none";
+    var display=getComputedStyle(document.getElementById("menu")).display;
+        if (display == "none") {
+            document.getElementById("menu").style = "display:block";
+        } else {
+            document.getElementById("menu").style = "display:none";
         }
 
-
     }
-}
+    window.onclick = function (event) {
+        if (!event.target.matches('.btn')) {
+            var dropdowns = document.getElementById("menu");
+            if (dropdowns.style.display == "block") {
+                dropdowns.style = "display:none";
+            }
 
-function seeLittleMenu() {
-    if (document.getElementById("littlemenu").style.display == "none") {
-        document.getElementById("littlemenu").style = "display:block";
-    } else {
-        document.getElementById("littlemenu").style = "display:none";
+
+        }
     }
-
-}
-window.onclick = function (event) {
-    if (!event.target.matches('.btn')) {
-        var dropdowns = document.getElementById("littlemenu");
-        if (dropdowns.style.display == "block") {
-            dropdowns.style = "display:none";
+    //screen size<768px
+    function seeLittleMenu() {
+        if (document.getElementById("littlemenu").style.display == "none") {
+            document.getElementById("littlemenu").style = "display:block";
+        } else {
+            document.getElementById("littlemenu").style = "display:none";
         }
 
-
     }
-}
+    window.onclick = function (event) {
+        if (!event.target.matches('.btn')) {
+            var dropdowns = document.getElementById("littlemenu");
+            if (dropdowns.style.display == "block") {
+                dropdowns.style = "display:none";
+            }
 
-function seeTooLittleMenu() {
-    if (document.getElementById("toolittlemenu").style.display == "none") {
-        document.getElementById("toolittlemenu").style = "display:block";
-    } else {
-        document.getElementById("toolittlemenu").style = "display:none";
+
+        }
     }
-
-}
-window.onclick = function (event) {
-    if (!event.target.matches('.btn')) {
-        var dropdowns = document.getElementById("toolittlemenu");
-        if (dropdowns.style.display == "block") {
-            dropdowns.style = "display:none";
+    //screen size<768px
+    function seeTooLittleMenu() {
+        if (document.getElementById("toolittlemenu").style.display == "none") {
+            document.getElementById("toolittlemenu").style = "display:block";
+        } else {
+            document.getElementById("toolittlemenu").style = "display:none";
         }
 
-
     }
-}
+    window.onclick = function (event) {
+        if (!event.target.matches('.btn')) {
+            var dropdowns = document.getElementById("toolittlemenu");
+            if (dropdowns.style.display == "block") {
+                dropdowns.style = "display:none";
+            }
+
+
+        }
+    }
